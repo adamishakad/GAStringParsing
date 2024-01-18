4b81f08d7878924b265eaf4f7b330f63220228d0
adamishakad <155333634+adamishakad@users.noreply.github.com>
2024-01-17 22:09:38 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GAStringParsing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
